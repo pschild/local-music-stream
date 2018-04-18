0040cec746bf1c4ba2492a3ec61ce5a5e73a132a
pschild <philippe.schild@gmail.com>
2018-04-18 17:39:59 +0200

    added basic authorization for search routes

--- server/routes/search/index.js
+++ server/routes/search/index.js
@@ -7,6 +7,11 @@ const searchRoute = require('express').Router();
 
 // TODO: remove
 searchRoute.post(`/`, (req, res) => {
+    if (!checkAuthorization(req.header('Authorization'))) {
+        res.status(401).send(`Invalid authorization param!`);
+        return;
+    }
+
     console.log('accessed /search with '+req.method+'@' + (new Date()).toTimeString());
     console.log(`body.payload: ${JSON.stringify(req.body.payload)}`);
     if (!req.body.payload) {
@@ -61,24 +66,44 @@ searchRoute.post(`/`, (req, res) => {
 });
 
 searchRoute.post(`/artist`, (req, res) => {
+    if (!checkAuthorization(req.header('Authorization'))) {
+        res.status(401).send(`Invalid authorization param!`);
+        return;
+    }
+
     console.log('accessed /search/artist');
     const payload = req.body.payload;
     res.json({ 'result': 'OK' });
 });
 
 searchRoute.post(`/artists`, (req, res) => {
+    if (!checkAuthorization(req.header('Authorization'))) {
+        res.status(401).send(`Invalid authorization param!`);
+        return;
+    }
+
     console.log('accessed /search/artists');
     const payload = req.body.payload;
     res.json({ 'result': 'OK' });
 });
 
 searchRoute.post(`/song`, (req, res) => {
+    if (!checkAuthorization(req.header('Authorization'))) {
+        res.status(401).send(`Invalid authorization param!`);
+        return;
+    }
+
     console.log('accessed /search/song');
     const payload = req.body.payload;
     res.json({ 'result': 'OK' });
 });
 
 searchRoute.post(`/songs`, (req, res) => {
+    if (!checkAuthorization(req.header('Authorization'))) {
+        res.status(401).send(`Invalid authorization param!`);
+        return;
+    }
+
     console.log('accessed /search/songs');
     const payload = req.body.payload;
     res.json({ 'result': 'OK' });
@@ -116,6 +141,18 @@ const getSongInformation = function(fileName) {
         artist: fileName.substr(0, separatorIndex).trim(),
         title: fileName.substr(separatorIndex + 1).trim()
     };
+};
+
+// TODO: put in class
+const checkAuthorization = function(authHeader) {
+    if (!authHeader) {
+        return false;
+    }
+
+    const authValue = authHeader.replace(/^Basic /, '');
+    const authTokenFromServerEnvironment = Buffer.from(`${process.env.LMS_USERNAME}:${process.env.LMS_PASSWORD}`).toString('base64');
+    console.log(authTokenFromServerEnvironment, authValue);
+    return authValue === authTokenFromServerEnvironment;
 };
 
 module.exports = searchRoute;
